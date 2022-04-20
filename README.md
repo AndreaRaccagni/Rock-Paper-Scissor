9a593e84e2ed0409b607455a00ec44356a1fddc8
Andrea <andrearaccagni81@gmail.com>
2022-04-20 13:44:43 +0200

    Add endGame section. Fixed playAgain button. Fixed style. Update readme file.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Rock-Paper-Scissor
 
-The classic game built with Javascript.
+The classic game built with Javascript Bishi Bashi version!
+
+This is my first FrontEnd project. The code is probably messy and ugly and the style is not so good. But it works fine!
--- game.js
+++ game.js
@@ -1,24 +1,29 @@
+
 const playerBtn = document.querySelectorAll('#player > .buttons > .btn');
 const cpuBtn = document.querySelectorAll('#cpu > .buttons > .btn');
-const resultDiv = document.querySelector('#show-result');
+const handWinnerDiv = document.querySelector('#show-hand-winner');
 const playerPoints = document.querySelector('#player-points');
 const cpuPoints = document.querySelector('#cpu-points');
+const containerDivs = document.querySelectorAll('#container div')
+const container = document.querySelector('#container')
 
 let playerScore = 0;
 let cpuScore = 0;
 
-// Button selection and result of a single game
+// Function that plays a single game
 playerBtn.forEach(e => e.addEventListener('click', (e) => {
     const player = e.target.id;
     const computer = computerPlay()
     const whoWin = playRound(player, computer);
     const actualScore = trackScore(whoWin);
     const outputText = document.createTextNode(`${whoWin}`);
-    resultDiv.replaceChildren(outputText);
+    handWinnerDiv.replaceChildren(outputText);
     highlightButton (playerBtn, player);
     highlightButton (cpuBtn, computer);
     playerPoints.replaceChildren(document.createTextNode(`${actualScore[0]}`));
     cpuPoints.replaceChildren(document.createTextNode(`${actualScore[1]}`));
+    const matchWinner = checkForWinner(actualScore[0], actualScore[1]);
+    if (matchWinner !== undefined) showWinner(matchWinner);
 }));
 
 // Function that returns the random cpu choice
@@ -68,19 +73,41 @@ function trackScore (handWinner) {
 // Function that checks for a winner
 function checkForWinner(playerScore, cpuScore) {
     if (playerScore === 5) {
-        // buttons disappear and a play again button pops in
-        return 'Awesome! You win!';
-    } else if (cpuScore === 5) {
-        // buttons disappear and a play again button pops in
-        return 'Darn it! You lose!';
+        return 'Awesome! You win!\n\nYou\'re a Rock-Paper-Scissor sensei master!';
+    } else if (cpuScore === 5) { 
+        return 'Darn it! You lose!\n\nYou need some more of "Wax on... Wax off, wax on... wax off"';
     };
 }
 
-
 // Function that highlight the selections
 function highlightButton (btnNodeList, selection) {
-    btnNodeList.forEach(element => element.style.background = "whitesmoke")
+    btnNodeList.forEach(element => element.style.background = "whitesmoke");
     for (let i = 0; i < btnNodeList.length; i++) {
-        if (btnNodeList[i].id === selection) btnNodeList[i].style.background = "#f0e973"
+        if (btnNodeList[i].id === selection) btnNodeList[i].style.background = "#f0e973";
     }
-}
+};
+
+// Function that ends the game
+function fadeOutContainer (...args) {
+    containerDivs.forEach(e => e.classList.add('fade-out'));
+    playerBtn.forEach(e => e.classList.add('block-button'));
+};
+
+// Function that shows the winner
+function showWinner (winner) {
+    fadeOutContainer(container, playerBtn);
+    const endGameDiv = document.createElement('div');
+    const matchWinnerDiv = document.createElement('div');
+    const replayBtn = document.createElement('button');
+    endGameDiv.appendChild(matchWinnerDiv);
+    endGameDiv.appendChild(replayBtn);
+    endGameDiv.classList.add('end-game');
+    matchWinnerDiv.classList.add('match-winner-text');
+    replayBtn.classList.add('button-86');
+    const matchWinnerText = document.createTextNode(`${winner}`);
+    const playAgainText = document.createTextNode(`PLAY AGAIN!`);
+    matchWinnerDiv.replaceChildren(matchWinnerText);
+    replayBtn.replaceChildren(playAgainText);
+    setTimeout(() => document.body.replaceChild(endGameDiv, container), 4000);
+    replayBtn.onclick = () => location.href = "index.html";
+};
